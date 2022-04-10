6bed04232b22109b7042a4ceed36d30a607e8f45
EthanC <zyyhaw@gmail.com>
2022-04-10 23:51:03 +0800

    update on 10/4/2022

---
+++ src/com/learningjava/AddNumbers.java
@@ -0,0 +1,19 @@
+package com.learningjava;
+
+/**
+ * Created on 10/04/2022.
+ */
+public class AddNumbers {
+
+    public static void main(String[] args) {
+        if (args[0].contains(".") || args[1].contains(".")) {
+            double paramDouble1 = Double.parseDouble(args[0]);
+            double paramDouble2 = Double.parseDouble(args[1]);
+            System.out.println(paramDouble1 + paramDouble2);
+        } else {
+            int paramInt1 = Integer.parseInt(args[0]);
+            int paramInt2 = Integer.parseInt(args[1]);
+            System.out.println(paramInt1 + paramInt2);
+        }
+    }
+}
---
+++ src/com/learningjava/AdminGuestMode.java
@@ -0,0 +1,22 @@
+package com.learningjava;
+
+import java.lang.String;
+/**
+ * Created on 10/04/2022.
+ */
+public class AdminGuestMode {
+    public static void main(String[] args) {
+        String inputArguments = String.join(",", args);
+        String adminFlag = "--admin";
+        String guestFlag = "--guest";
+        if (inputArguments.contains(adminFlag) && inputArguments.contains(guestFlag)) {
+            System.out.println("Please, select either 'ADMIN' or "
+                    + "'GUEST' mode for this program");
+        } else if (inputArguments.contains(adminFlag)) {
+            System.out.println("Hello, Admin!");
+        } else if (inputArguments.contains(guestFlag)) {
+            System.out.println("Hello, Guest!");
+        }
+
+    }
+}
